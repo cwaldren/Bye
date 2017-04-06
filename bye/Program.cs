c6bcca7056879184552b99e32e1063d0a3601fc5
Casey Waldren <casey@nullspacevr.com>
2017-04-05 22:25:44 -0700

    Add project files.

---
+++ bye/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
