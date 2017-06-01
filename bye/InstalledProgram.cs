e996d627d2c14de8e7f143c06d44999e0a0b8cf5
Casey Waldren <casey@nullspacevr.com>
2017-05-31 21:07:34 -0700

    Add licence; slight cleanup

---
+++ bye/InstalledProgram.cs
@@ -0,0 +1,447 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+/// <summary>
+/// Represents a program that is installed on a PC
+/// </summary>
+public class InstalledProgram : IComparable<InstalledProgram>, IEquatable<InstalledProgram>
+{
+
+
+    #region "Properties"
+
+    private string _DisplayName = string.Empty;
+    /// <summary>
+    /// The name that would be displayed in Add/Remove Programs
+    /// </summary>
+    public string DisplayName
+    {
+        get { return _DisplayName; }
+        set { _DisplayName = value; }
+    }
+
+    private string _Version = string.Empty;
+    /// <summary>
+    /// The version of the program
+    /// </summary>
+    public string Version
+    {
+        get { return _Version; }
+        set { _Version = value; }
+    }
+
+    private string _ParentDisplayName = string.Empty;
+    /// <summary>
+    /// If this program is an update then this will contain the display name of the product that 
+    /// this is an update to
+    /// </summary>
+    public string ParentDisplayName
+    {
+        get { return _ParentDisplayName; }
+        set { _ParentDisplayName = value; }
+    }
+
+    private bool _IsUpdate;
+    /// <summary>
+    /// Is this program classed as an update 
+    /// </summary>
+    public bool IsUpdate
+    {
+        get { return _IsUpdate; }
+        set { _IsUpdate = value; }
+    }
+
+    private string _UninstallString;
+    public string UninstallString
+    {
+        get { return _UninstallString; }
+       set { _UninstallString = value; }
+    }
+
+    private string _Guid;
+    public string Guid
+    {
+        get { return _Guid; }
+        set { _Guid = value; }
+    }
+    #endregion
+
+    #region "Constructors"
+
+    public InstalledProgram(string ProgramDisplayName, string UninstallString)
+    {
+        this.DisplayName = ProgramDisplayName;
+        this._UninstallString = UninstallString;
+    }
+
+    public InstalledProgram(string ProgramDisplayName)
+    {
+        this.DisplayName = ProgramDisplayName;
+    }
+    public InstalledProgram(string ProgramDisplayName, string ProgramParentDisplayName, bool IsProgramUpdate, string ProgramVersion)
+    {
+        this.DisplayName = ProgramDisplayName;
+        this.ParentDisplayName = ProgramParentDisplayName;
+        this.IsUpdate = IsProgramUpdate;
+        this.Version = ProgramVersion;
+    }
+
+    #endregion
+
+    #region "Public Methods"
+
+    public override string ToString()
+    {
+        return DisplayName;
+    }
+
+    /// <summary>
+    /// Retrieves a list of all installed programs on the local computer
+    /// </summary>
+    public static List<InstalledProgram> GetInstalledPrograms(bool IncludeUpdates)
+    {
+        return InternalGetInstalledPrograms(IncludeUpdates, Registry.LocalMachine, Registry.Users);
+    }
+
+    /// <summary>
+    /// Retrieves a list of all installed programs on the specified computer
+    /// </summary>
+    /// <param name="ComputerName">The name of the computer to get the list of installed programs from</param>
+    /// <param name="IncludeUpdates">Determines whether or not updates for installed programs are included in the list</param>
+    public static List<InstalledProgram> GetInstalledPrograms(string ComputerName, bool IncludeUpdates)
+    {
+        try
+        {
+            return InternalGetInstalledPrograms(IncludeUpdates, RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, ComputerName), RegistryKey.OpenRemoteBaseKey(RegistryHive.Users, ComputerName));
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return new List<InstalledProgram>();
+        }
+    }
+
+    //Sorting function, required by IComparable interface
+    public int CompareTo(InstalledProgram other)
+    {
+        return string.Compare(this.DisplayName, other.DisplayName);
+    }
+
+    //Equality function, required by IEquatable interface
+    public bool Equals1(InstalledProgram other)
+    {
+        if (this.DisplayName == other.DisplayName && this.Version == other.Version)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    bool System.IEquatable<InstalledProgram>.Equals(InstalledProgram other)
+    {
+        return Equals1(other);
+    }
+
+    #endregion
+
+    #region "Private Methods"
+
+    private static List<InstalledProgram> InternalGetInstalledPrograms(bool IncludeUpdates, RegistryKey HklmPath, RegistryKey HkuPath)
+    {
+        List<InstalledProgram> ProgramList = new List<InstalledProgram>();
+
+        RegistryKey ClassesKey = HklmPath.OpenSubKey("Software\\Classes\\Installer\\Products");
+
+        //---Wow64 Uninstall key
+        RegistryKey Wow64UninstallKey = HklmPath.OpenSubKey("Software\\Wow6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+        ProgramList = GetUninstallKeyPrograms(Wow64UninstallKey, ClassesKey, ProgramList, IncludeUpdates);
+
+        //---Standard Uninstall key
+        RegistryKey StdUninstallKey = HklmPath.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+        ProgramList = GetUninstallKeyPrograms(StdUninstallKey, ClassesKey, ProgramList, IncludeUpdates);
+
+        foreach (string UserSid in HkuPath.GetSubKeyNames())
+        {
+            //---HKU Uninstall key
+            RegistryKey CuUnInstallKey = HkuPath.OpenSubKey(UserSid + "\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall");
+            ProgramList = GetUninstallKeyPrograms(CuUnInstallKey, ClassesKey, ProgramList, IncludeUpdates);
+            //---HKU Installer key
+            RegistryKey CuInstallerKey = HkuPath.OpenSubKey(UserSid + "\\Software\\Microsoft\\Installer\\Products");
+            ProgramList = GetUserInstallerKeyPrograms(CuInstallerKey, HklmPath, ProgramList);
+        }
+
+        //Close the registry keys
+        try
+        {
+            HklmPath.Close();
+            HkuPath.Close();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine("Error closing registry key - " + ex.Message);
+        }
+
+        //Sort the list alphabetically and return it to the caller
+        //ProgramList.Sort();
+        return ProgramList;
+    }
+
+    private static bool IsProgramInList(string ProgramName, List<InstalledProgram> ListToCheck)
+    {
+        return ListToCheck.Contains(new InstalledProgram(ProgramName));
+    }
+
+    private static List<InstalledProgram> GetUserInstallerKeyPrograms(RegistryKey CuInstallerKey, RegistryKey HklmRootKey, List<InstalledProgram> ExistingProgramList)
+    {
+        if ((CuInstallerKey != null))
+        {
+            foreach (string CuProductGuid in CuInstallerKey.GetSubKeyNames())
+            {
+                bool ProductFound = false;
+                var data = HklmRootKey.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData");
+                if (data == null)
+                {
+                    continue;
+                }
+                foreach (string UserDataKeyName in data.GetSubKeyNames())
+                {
+                    //Ignore the LocalSystem account
+                    if (!(UserDataKeyName == "S-1-5-18"))
+                    {
+                        RegistryKey ProductsKey = HklmRootKey.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\" + UserDataKeyName + "\\Products");
+                        if ((ProductsKey != null))
+                        {
+                            string[] LmProductGuids = ProductsKey.GetSubKeyNames();
+                            foreach (string LmProductGuid in LmProductGuids)
+                            {
+                                if (LmProductGuid == CuProductGuid)
+                                {
+                                    RegistryKey UserDataProgramKey = HklmRootKey.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Installer\\UserData\\" + UserDataKeyName + "\\Products\\" + LmProductGuid + "\\InstallProperties");
+                                    if (!(Convert.ToInt32(UserDataProgramKey.GetValue("SystemComponent", 0)) == 1))
+                                    {
+                                        string Name = Convert.ToString(CuInstallerKey.OpenSubKey(CuProductGuid).GetValue("ProductName", string.Empty));
+                                        string Uninstall = Convert.ToString(CuInstallerKey.OpenSubKey(CuProductGuid).GetValue("UninstallString", string.Empty));                                        string ProgVersion = string.Empty;
+                                        
+                                        try
+                                        {
+                                            ProgVersion = Convert.ToString(UserDataProgramKey.GetValue("DisplayVersion", string.Empty));
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Debug.WriteLine(ex.Message);
+                                        }
+                                        if (!(Name == string.Empty) && !IsProgramInList(Name, ExistingProgramList))
+                                        {
+                                            var prog = new InstalledProgram(Name, Uninstall);
+                                          var guid= System.Guid.Parse(LmProductGuid);
+                                            var temp= guid.ToString("B");
+                                            var real = System.Guid.Parse(GetInstallerKeyNameFromGuid(temp)).ToString("B");
+                                            prog.Guid = real;
+                                            ExistingProgramList.Add(prog);
+                                            ProductFound = true;
+                                        }
+                                    }
+                                    break; // TODO: might not be correct. Was : Exit For
+                                }
+                            }
+                            if (ProductFound)
+                            {
+                                break; // TODO: might not be correct. Was : Exit For
+                            }
+                            try
+                            {
+                                ProductsKey.Close();
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ExistingProgramList;
+    }
+
+    private static List<InstalledProgram> GetUninstallKeyPrograms(RegistryKey UninstallKey, RegistryKey ClassesKey, List<InstalledProgram> ExistingProgramList, bool IncludeUpdates)
+    {
+        //Make sure the key exists
+        if ((UninstallKey != null))
+        {
+            //Loop through all subkeys (each one represents an installed program)
+            foreach (string SubKeyName in UninstallKey.GetSubKeyNames())
+            {
+                try
+                {
+                    RegistryKey CurrentSubKey = UninstallKey.OpenSubKey(SubKeyName);
+                    //Skip this program if the SystemComponent flag is set
+                    int IsSystemComponent = 0;
+                    bool ErrorCheckingSystemComponent = false;
+                    try
+                    {
+                        IsSystemComponent = Convert.ToInt32(CurrentSubKey.GetValue("SystemComponent", 0));
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.WriteLine(SubKeyName + " - " + ex.Message);
+                    }
+                    if (!(IsSystemComponent == 1))
+                    {
+                        //If the WindowsInstaller flag is set then add the key name to our list of Windows Installer GUIDs
+                        if (!(Convert.ToInt32(CurrentSubKey.GetValue("WindowsInstaller", 0)) == 1))
+                        {
+                            System.Text.RegularExpressions.Regex WindowsUpdateRegEx = new System.Text.RegularExpressions.Regex("KB[0-9]{6}$");
+                            string ProgramReleaseType = Convert.ToString(CurrentSubKey.GetValue("ReleaseType", string.Empty));
+                            string ProgVersion = string.Empty;
+                            try
+                            {
+                                ProgVersion = Convert.ToString(CurrentSubKey.GetValue("DisplayVersion", string.Empty));
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(SubKeyName + " - " + ex.Message);
+                            }
+                            //Check to see if this program is classed as an update
+                            if (WindowsUpdateRegEx.Match(SubKeyName).Success == true || !(Convert.ToString(CurrentSubKey.GetValue("ParentKeyName", string.Empty)) == string.Empty) || ProgramReleaseType == "Security Update" || ProgramReleaseType == "Update Rollup" || ProgramReleaseType == "Hotfix")
+                            {
+                                if (IncludeUpdates)
+                                {
+                                    //Add the program to our list if we are including updates in this search
+                                    string Name = Convert.ToString(CurrentSubKey.GetValue("DisplayName", string.Empty));
+                                    string uninstall = Convert.ToString(CurrentSubKey.GetValue("UninstallString", string.Empty));
+                                        
+                                    if (!(Name == string.Empty) && !IsProgramInList(Name, ExistingProgramList))
+                                    {
+                                      var ip= new InstalledProgram(Name, Convert.ToString(CurrentSubKey.GetValue("ParentDisplayName", string.Empty)), true, ProgVersion);
+                                        ip.UninstallString = uninstall;
+                                        ExistingProgramList.Add(ip);
+
+                                    }
+                                }
+                                //If not classed as an update
+                            }
+                            else
+                            {
+                                bool UninstallStringExists = false;
+                                foreach (string valuename in CurrentSubKey.GetValueNames())
+                                {
+                                    if (string.Compare("UninstallString", valuename, true) == 0)
+                                    {
+                                        UninstallStringExists = true;
+                                        break; // TODO: might not be correct. Was : Exit For
+                                    }
+                                }
+                                if (UninstallStringExists)
+                                {
+                                    string Name = Convert.ToString(CurrentSubKey.GetValue("DisplayName", string.Empty));
+                                    string uninstall = Convert.ToString(CurrentSubKey.GetValue("UninstallString", string.Empty));
+                                    if (!(Name == string.Empty) && !IsProgramInList(Name, ExistingProgramList))
+                                    {
+                                        var ip=new InstalledProgram(Name, Convert.ToString(CurrentSubKey.GetValue("ParentDisplayName", string.Empty)), false, ProgVersion);
+                                        ip.UninstallString = uninstall;
+                                        ExistingProgramList.Add(ip);
+                                    }
+                                }
+                            }
+                            //If WindowsInstaller
+                        }
+                        else
+                        {
+                            string ProgVersion = string.Empty;
+                            string Name = string.Empty;
+                            string Uninstall = string.Empty;
+                            string Guid = string.Empty;
+                            bool FoundName = false;
+                            try
+                            {
+                                string MsiKeyName = GetInstallerKeyNameFromGuid(SubKeyName);
+                                RegistryKey CrGuidKey = ClassesKey.OpenSubKey(MsiKeyName);
+                                if ((CrGuidKey != null))
+                                {
+                                    Name = Convert.ToString(CrGuidKey.GetValue("ProductName", string.Empty));
+                                    Uninstall = Convert.ToString(CurrentSubKey.GetValue("UninstallString", string.Empty));
+                                    Guid = SubKeyName;
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(SubKeyName + " - " + ex.Message);
+                            }
+                            try
+                            {
+                                ProgVersion = Convert.ToString(CurrentSubKey.GetValue("DisplayVersion", string.Empty));
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                            }
+                            if (!(Name == string.Empty) && !IsProgramInList(Name, ExistingProgramList))
+                            {
+                                var ip = new InstalledProgram(Name, Convert.ToString(CurrentSubKey.GetValue("ParentDisplayName", string.Empty)), false, ProgVersion);
+                                ip.UninstallString = Uninstall;
+                                ip.Guid = Guid;
+                                ExistingProgramList.Add(ip);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(SubKeyName + " - " + ex.Message);
+                }
+            }
+            //Close the registry key
+            try
+            {
+                UninstallKey.Close();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+        return ExistingProgramList;
+    }
+
+    private static string GetInstallerKeyNameFromGuid(string GuidName)
+    {
+        string[] MsiNameParts = GuidName.Replace("{", "").Replace("}", "").Split('-');
+        System.Text.StringBuilder MsiName = new System.Text.StringBuilder();
+        //Just reverse the first 3 parts
+        for (int i = 0; i <= 2; i++)
+        {
+            MsiName.Append(ReverseString(MsiNameParts[i]));
+        }
+        //For the last 2 parts, reverse each character pair
+        for (int j = 3; j <= 4; j++)
+        {
+            for (int i = 0; i <= MsiNameParts[j].Length - 1; i++)
+            {
+                MsiName.Append(MsiNameParts[j][i + 1]);
+                MsiName.Append(MsiNameParts[j][i]);
+                i += 1;
+            }
+        }
+        return MsiName.ToString();
+    }
+
+    private static string ReverseString(string input)
+    {
+        char[] Chars = input.ToCharArray();
+        Array.Reverse(Chars);
+        return new string(Chars);
+    }
+
+    #endregion
+
+
+}
--- bye/Program.cs
+++ bye/Program.cs
@@ -1,15 +1,246 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FuzzyString;
 
 namespace bye
 {
+
     class Program
     {
+        interface Matcher
+        {
+            bool Match(InstalledProgram program, SearchTerm term);
+        }
+
+        class RankingPair : IComparable
+        {
+            public InstalledProgram Program;
+            public int LevenDistance;
+
+            public RankingPair(InstalledProgram p, int levenDistance)
+            {
+                Program = p;
+                LevenDistance = levenDistance;
+            }
+
+            public int CompareTo(object other)
+            {
+                return LevenDistance.CompareTo(((RankingPair)other).LevenDistance);
+            }
+        }
+
+        class SearchTerm
+        {
+            private string originalTerm;
+            private string[] normalizedTokens;
+            private string fusedTerm;
+
+            public string Original
+            {
+                get { return originalTerm; }
+            }
+
+            public string[] Tokens
+            {
+                get { return normalizedTokens; }
+            }
+
+            public string Fused
+            {
+                get { return fusedTerm; }
+            }
+
+            public string Normalized
+            {
+                get { return Normalize(originalTerm); }
+            }
+
+            
+
+            public SearchTerm(string query)
+            {
+                originalTerm = query;
+                string normalized = Normalize(query);
+
+                normalizedTokens = normalized.Split(' ');
+                fusedTerm = normalized.Replace(" ", string.Empty);
+
+            }
+
+            private string Normalize(string query)
+            {
+                return query.ToLower();
+            }
+
+
+        }
+
+        class FuzzyMatcher : Matcher
+        {
+            private List<FuzzyStringComparisonOptions> options;
+
+            public FuzzyMatcher()
+            {
+                options = new List<FuzzyStringComparisonOptions>();
+                options.Add(FuzzyStringComparisonOptions.UseLongestCommonSubstring);
+            }
+
+            public bool Match(InstalledProgram program, SearchTerm term)
+            {
+                var programNameTokens = program.DisplayName
+                    .Trim()
+                    .Split(' ')
+                    .Where(result => result.Length > 0)
+                    .Select(raw => raw.ToLower());
+
+                foreach (var token in programNameTokens)
+                {
+                    if (token.ApproximatelyEquals(term.Original, options, FuzzyStringComparisonTolerance.Strong)
+                      ||token.ApproximatelyEquals(term.Fused, options, FuzzyStringComparisonTolerance.Strong))
+                    {
+                        return true;
+                    }
+
+                }
+
+                return false;
+
+            }
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Usage: bye [progam-name]\nEx: bye chrome\n");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args)
         {
+            
+            if (args.Length < 1)
+            {
+                PrintUsage();
+                return;
+            }
+
+            string searchTerm = args[0];
+
+            if (searchTerm.Length == 0)
+            {
+                PrintUsage();
+                return;
+            }
+
+            SearchTerm search = new SearchTerm(searchTerm);
+            
+            var programs = InstalledProgram.GetInstalledPrograms(false);
+
+            HashSet<InstalledProgram> candidates = new HashSet<InstalledProgram>();
+
+
+            Matcher matcher = new FuzzyMatcher();
+
+
+            foreach (var program in programs)
+            {
+                if (matcher.Match(program, search))
+                {
+                    candidates.Add(program);
+                }
+            }
+
+            var candidateList = candidates.ToList();
+          
+            if (candidateList.Count == 1)
+            {
+                var uninstallCandidate = candidateList.Single();
+                Console.WriteLine("Uninstall " + uninstallCandidate.DisplayName + "?");
+                if (Console.ReadLine().Contains("y"))
+                {
+                    Uninstall(uninstallCandidate);
+                }
+                return;
+            }
+            else if (candidateList.Count > 1)
+            {
+                var levens = new List<RankingPair>(candidateList.Count);
+                foreach (var candidate in candidateList)
+                {
+                    var parts = candidate.DisplayName.Trim().Split(' ').Where(raw => raw.Length > 0).ToArray();
+                  
+                    int partsCountHeuristic = Math.Abs(search.Tokens.Length - parts.Count());
+
+                    //int lowestDist = int.MaxValue;
+                    //for (int i = 0; i < parts.Length; i++)
+                    //{
+                    //    lowestDist = Math.Min(lowestDist, parts[i].LevenshteinDistance(search.Original));
+                    //}
+
+                    int lowestDist = parts.Min(part => part.LevenshteinDistance(search.Original));
+                    levens.Add(new RankingPair(candidate, lowestDist + partsCountHeuristic));
+                }
+
+                levens.Sort();
+                Console.WriteLine("Which?");
+
+                for (int i = 0; i < levens.Count; i++)
+                {
+                    Console.WriteLine(string.Format("[{0}] {1}", i, levens[i].Program.DisplayName));
+                }
+                int result = 0;
+                if(int.TryParse(Console.ReadLine(), out result))
+                {
+                    Uninstall(levens[result].Program);
+                    
+                }else
+                {
+                    Console.WriteLine("Write a number next time, please!");
+                }
+            } else
+            {
+                Console.WriteLine("Nothing found. Bye.");
+            }
+        }
+
+        private static void Uninstall(InstalledProgram program)
+        {
+            if (program.Guid != null && program.Guid.Length > 0)
+            {
+                UninstallFromGuid(program.Guid);
+            } else if (program.UninstallString.Length > 0)
+            {
+                UninstallFromUninstaller(program.UninstallString);
+            } else
+            {
+                Console.WriteLine("I'm sorry, I couldn't figure out how to uninstall it");
+            }
+          
+        }
+
+        private static void UninstallFromUninstaller(string uninstallString)
+        {
+
+            System.Diagnostics.Process.Start(uninstallString);
+
+            Console.WriteLine("..bye");
+        
+
+        }
+
+        private static void UninstallFromGuid(string guid)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "msiexec.exe";
+            startInfo.Arguments = "/x " + guid + " /promptrestart";
+          //  Console.WriteLine("msiexec.exe with args: " + startInfo.Arguments);
+
+            process.StartInfo = startInfo;
+            process.Start();
+            process.WaitForExit();
+            Console.WriteLine("..bye");
         }
     }
 }
